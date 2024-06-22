e50842ab49e7a1b5f04509e6ecaf0ad32e6044fc
kim0123 <166187340+XDCRF5BGHTVY6@users.noreply.github.com>
2024-06-22 22:52:37 +0900

    Update ClipNote_Plus.py

--- ClipNote_Plus.py
+++ ClipNote_Plus.py
@@ -6,20 +6,20 @@ window = tkinter.Tk()
 window.title('Notes Plus')
 window.geometry('400x500+100+100')
 
-#쓰레드 생성
+
 def th():
     th = threading.Thread(target=clipture)
     th.daemon = True
     th.start()
 
-#화면 고정
+
 def pined():
     if cv1.get()==1:
         window.wm_attributes("-topmost",1)
     else:
         window.wm_attributes("-topmost",0)
 
-#클립보드 연동
+
 def clipture():
     result2=""
     while True:
@@ -34,30 +34,30 @@ def clipture():
             clipdelet.config(command=lambda cl=clippaste , dl=clipdelet : clip_delet(cl,dl))
         if cv2.get() != 1 :
             break
-#삭제
+
 def clip_delet(cl,dl):
     dl.destroy()
     cl.destroy()
     
 
-#메모 추가
+
 def add_meom1():
     meom=tkinter.Text(window,width=50,height=10)
     meom.pack()
     delet=tkinter.Button(window,text='X',command=lambda: (meom.destroy(),delet.destroy()))
     delet.pack()
 
-#pin 체크박스
+
 cv1=tkinter.IntVar()
 pin=tkinter.Checkbutton(window,text="고정",variable=cv1,command=pined)
 pin.pack()
 
-#클립보드 체크박스
+
 cv2=tkinter.IntVar()
 clip=tkinter.Checkbutton(window,text="클립보드 연동",variable=cv2,command=th)
 clip.pack()
 
-#메모추가 버튼
+
 add_memo=tkinter.Button(window,text='+',width=30,command=add_meom1)
 add_memo.pack()
 
